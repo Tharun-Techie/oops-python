52c2e01149e907c2057646ce9185623adb985589
Tharun R <Tharuntechie@gmail.com>
2024-10-05 11:26:33 +0530

    classes and objects added

---
+++ class_obj.py
@@ -0,0 +1,12 @@
+class Human:
+    Name = None
+    Age = None
+    Weight = None
+    Height = None
+    
+    
+person1 = Human()
+print(person1)
+person1 = person1
+person1 = Human()
+print(person1)
